d7aff6efe5a43e19455d446a1b6e783d9fecb510
Mohamed Refaat <58652163+mohamed24119@users.noreply.github.com>
2023-09-22 17:35:59 +0300

    Update author-profile.js

--- author-profile.js
+++ author-profile.js
@@ -2,7 +2,7 @@ function fun_get_card_author_des(current_post_id) {
     const card_author_img = document.querySelector("#card-author-img");
     const card_author_des = document.querySelector("#card-author-des");
     if(card_author_img !== null){
-	    const url_author_img = `https://www.blogger.com/feeds/${$blog_id}/posts/default/${current_post_id}?alt=json`;
+	    const url_author_img = `https://www.blogger.com/feeds/${blog_id}/posts/default/${current_post_id}?alt=json`;
 	    fetch(url_author_img)
         .then(function(response) {return response.json()})
         .then(function(data) {
